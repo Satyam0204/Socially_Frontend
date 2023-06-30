d79664cf557d039ba45c7159c8d77684e78d3b6a
Satyam0204 <satyampattanaik007@gmail.com>
2023-07-01 02:18:51 +0530

    [STYLE] added grid layout

--- src/App.js
+++ src/App.js
@@ -13,19 +13,26 @@ import Register from "./pages/Register";
 import PostPage from "./pages/PostPage";
 import UserPage from "./pages/UserPage";
 import ProfilePage from "./pages/ProfilePage";
+import Navbar from "./components/Navbar/Navbar";
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-gradient-to-r from-[#2c074f]   to-[#251c29] ">
       <Router>
         <AuthProvider>
 
         <Header/>
-      
-        <Routes>
+        <div className="grid grid-cols-4">
+        <div className="col-span-1">
+
+        <Navbar />
+        </div>
+      <div className='col-span-2'>
+
+        <Routes >
         
-          <Route exact path='/' element={<PrivateRoute><Home/></PrivateRoute>}/>          
+          <Route  exact path='/' element={<PrivateRoute><Home/></PrivateRoute>}/>          
           <Route path='/post/:id' element={<PrivateRoute><PostPage/></PrivateRoute>}/>         
           <Route path='/user' element={<PrivateRoute><UserPage/></PrivateRoute>}/>         
           <Route path='/profile/:id' element={<PrivateRoute><ProfilePage/></PrivateRoute>}/>         
@@ -33,6 +40,8 @@ function App() {
           <Route path='/register' element={<Register />} />          
 
         </Routes>
+      </div>
+        </div>
         </AuthProvider>
       </Router>
     </div>
